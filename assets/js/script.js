cf5d28cf15144f675e0d5d403b00b95c9ab6af40
HarrisonT1 <harrisontownend@hotmail.com>
2025-07-14 21:47:06 +0100

    Add goodLuckMessage function with timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,36 @@
+let optionsContainer = document.getElementById("options-container")
+let options = document.getElementsByClassName("option")
+let optionsArray = Array.from(options)
+let score = document.getElementById("score")
+let heading = document.querySelector("#heading h2")
+
 document.addEventListener("DOMContentLoaded", function() {
 
   for (let option of options) {
     option.addEventListener("click", function() {
-      selectTopic();
-      goodLuckMessage();
+      hideHomepage();
+      goodLuckMessage(option);
+      setTimeout(() => {
+        heading.textContent = "";
+      }, 2000);
     })
   }
 });
 
-let optionsContainer = document.getElementById("options-container")
-let options = document.getElementsByClassName("option")
-let score = document.getElementById("score")
-let heading = document.getElementById("heading")
 
-function goodLuckMessage() {
-  // if (option === )
+function goodLuckMessage(optionChoice) {
+  if (optionChoice === optionsArray[0]) {
+    heading.textContent = "Good Luck! You chose General Knowledge"
+  }
 }
+
 function selectTopic() {
+
+}
+function hideHomepage() {
   optionsContainer.classList.remove("options-container")
   optionsContainer.classList.add("hidden")
   score.classList.add("hidden")
 }
-function hideHomepage() {}
 function increamentScore() {}
 function correctAnswer() {}

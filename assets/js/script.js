ee8c50b3c708cadfcdd83dbde53aa3fe0c099201
HarrisonT1 <harrisontownend@hotmail.com>
2025-07-14 21:00:47 +0100

    Add funciton to remove options from homepage when topic is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+  for (let option of options) {
+    option.addEventListener("click", function() {
+      selectTopic();
+      goodLuckMessage();
+    })
+  }
+});
 
-function selectTopic() {}
+let optionsContainer = document.getElementById("options-container")
+let options = document.getElementsByClassName("option")
+let score = document.getElementById("score")
+let heading = document.getElementById("heading")
+
+function goodLuckMessage() {
+  // if (option === )
+}
+function selectTopic() {
+  optionsContainer.classList.remove("options-container")
+  optionsContainer.classList.add("hidden")
+  score.classList.add("hidden")
+}
 function hideHomepage() {}
 function increamentScore() {}
 function correctAnswer() {}
